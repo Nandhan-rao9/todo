f554e8af2953ee8df287e8f2f48ccab8052918b2
Nandhan_9 <184651839+Nandhan-rao9@users.noreply.github.com>
2025-10-05 13:26:22 +0530

    unit testing

--- backend/test_app.py
+++ backend/test_app.py
@@ -1,14 +1,17 @@
 # backend/test_app.py
 import pytest
-from app import app as flask_app # Import your Flask app instance
+from app import app as flask_app  # Import your Flask app instance
+
 
 @pytest.fixture()
 def app():
     yield flask_app
 
+
 @pytest.fixture()
 def client(app):
     return app.test_client()
+
 
 def test_hello_world(client):
     """A simple test to ensure a non-existent route returns 404."""
